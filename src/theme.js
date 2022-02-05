b0546977a4623ab817332635994b3d35058cada4
purple402 <purple402@naver.com>
2022-02-05 15:23:43 +0900

    Use ThemeProvider in App.js

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+const Colors = {
+  white: 'rgb(255, 255, 255)',
+  black: 'rgb(0, 0, 0)',
+  background: 'rgb(255, 232, 191)',
+  main: 'rgb(51, 65, 159)',
+  red: 'crimson',
+  gray: 'gray',
+};
+
+const Fonts = {};
+
+export const theme = {
+  background: Colors.background,
+  main: Colors.main,
+  red: Colors.red,
+  gray: Colors.gray,
+  black: Colors.black,
+};
