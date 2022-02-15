0d7e2a307123e4f87547d2be50f942b4d8ade7ed
Joy Lee <purple402@naver.com>
2022-02-15 20:36:04 +0900

    Create Progress Pie in Timer screen

--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "styled-components/native";
 import styled from "styled-components/native";
+import * as Progress from "react-native-progress";
+
 const Container = styled.View`
   background-color: ${({ theme }) => theme.background};
   width: 100%;
@@ -14,11 +16,35 @@ const Title = styled.Text`
   margin: 10px 0 30px 0;
 `;
 
+const PieContainer = styled.TouchableOpacity``;
 const Timer = () => {
   const theme = useContext(ThemeContext);
+  const [progress, setProgress] = useState(0.3);
+  const getAngle = (x, y) => {
+    let angle = 0;
+    if (x >= 0) {
+      angle = 90 - Math.atan(y / x) * (180 / Math.PI);
+    } else {
+      angle = 270 - Math.atan(y / x) * (180 / Math.PI);
+    }
+    return angle;
+  };
+
+  const setTime = (e) => {
+    const { locationX, locationY } = e.nativeEvent;
+    const x = locationX - 150;
+    const y = 150 - locationY;
+
+    const newProgress = getAngle(x, y) / 360;
+    setProgress(newProgress);
+  };
+
   return (
     <Container>
       <Title>Timer Page</Title>
+      <PieContainer onPress={setTime}>
+        <Progress.Pie progress={progress} size={300} color={theme.red} />
+      </PieContainer>
     </Container>
   );
 };
