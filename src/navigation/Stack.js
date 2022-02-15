bc9d15b1e56664222de47a918baad008ab65b81a
Joy Lee <purple402@naver.com>
2022-02-15 20:34:37 +0900

    Create Stack Navigation

---
+++ src/navigation/Stack.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "styled-components/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Main, Timer } from "../screens";
+
+const Stack = createNativeStackNavigator();
+
+const StackNav = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <Stack.Navigator initialRouteName="Timer">
+      {/* Stack.Navigatior 안에 Screen이 있어야한다 */}
+      {/* 필수 props는 name, component */}
+      <Stack.Screen
+        name="Home"
+        component={Main}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Timer"
+        component={Timer}
+        options={{
+            title: "",
+            headerStyle: {
+              backgroundColor: theme.background,
+            },
+            headerShadowVisible: false,
+            headerTintColor: theme.main,
+            headerBackTitleVisible: false,
+          }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNav;
