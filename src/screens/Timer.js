167a1ce9cb00d92a8373b584fe6d5b3691649b29
Joy Lee <purple402@naver.com>
2022-02-19 00:18:35 +0900

    Create timer buttons

--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "styled-components/native";
 import styled from "styled-components/native";
 import * as Progress from "react-native-progress";
+import { Alert, View } from "react-native";
 
 const Container = styled.View`
   background-color: ${({ theme }) => theme.background};
@@ -13,11 +14,45 @@ const Container = styled.View`
 const Title = styled.Text`
   color: ${({ theme }) => theme.main};
   font-size: 40px;
-  margin: 10px 0 30px 0;
+  margin: 0 0 30px 0;
+  font-family: ${({ theme }) => theme.fontSub};
 `;
 
-const PieContainer = styled.TouchableOpacity``;
-const ClockContainer = styled.TouchableOpacity``;
+// 시간 설정 버튼
+const TimerButtonContainer = styled.View`
+  width: 340px;
+  height: 340px;
+  border-radius: 170px;
+  /* border: 1px solid ${({ theme }) => theme.gray}; */
+  background-color: ${({ theme }) => theme.paleWhite};
+`;
+
+const TimerButton = styled.TouchableOpacity`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: ${({ theme, selected }) =>
+    selected ? theme.main : theme.transparent};
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => left}px;
+`;
+
+const StartButton = styled(TimerButton)`
+  width: 84px;
+  height: 84px;
+  border-radius: 42px;
+  background-color: ${({ theme }) => theme.red};
+`;
+
+const ButtonText = styled.Text`
+  font-size: 23px;
+  color: ${({ theme, selected }) => (selected ? theme.white : theme.main)};
+  font-family: ${({ theme }) => theme.fontSub};
+`;
+
 const PieContainer = styled.View``;
 const ClockContainer = styled.View``;
 
@@ -28,32 +63,81 @@ const ClockNumber = styled.Text`
   font-family: ${({ theme }) => theme.fontSub};
 `;
 
-const Timer = () => {
+const Timer = ({ navigation, route }) => {
+  const { id, text } = route.params;
+  const settingTime = 25; // 처음 시간
   const theme = useContext(ThemeContext);
-  const [progress, setProgress] = useState(0.3);
-  const [minute, setMinute] = useState(progress * 60);
+  const [progress, setProgress] = useState(1);
+  const [time, setTime] = useState(settingTime * 60);
+  const [initialTime, setInitialTime] = useState(settingTime * 60);
+  const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
   const [usePie, setUsePie] = useState(false); // Pie사용 여부
+  const [isCounting, setIsCounting] = useState(false); // 시계 시작 여부
 
-  const getAngle = (x, y) => {
-    let angle = 0;
-    if (x >= 0) {
-      angle = 90 - Math.atan(y / x) * (180 / Math.PI);
-    } else {
-      angle = 270 - Math.atan(y / x) * (180 / Math.PI);
+  useEffect(() => {
+    let interval = null;
+    if (isCounting) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 100);
     }
-    return angle;
-  };
 
-  const setTime = (e) => {
-    const { locationX, locationY } = e.nativeEvent;
-    const x = locationX - 150;
-    const y = 150 - locationY;
+    return () => clearInterval(interval);
+  }, [isCounting]);
 
-    const newProgress = getAngle(x, y) / 360;
-    setProgress(newProgress);
-    setMinute(Math.floor(newProgress * 60));
-    setSecond(0);
+  useEffect(() => {
+    if (time > 0) {
+      setMinute(parseInt(time / 60));
+      setSecond(time % 60);
+      const newProgress = time / initialTime;
+      setProgress(newProgress);
+    } else if (time === 0) {
+      Alert.alert(null, "할일을 완료할까요?", [
+        {
+          text: "계속하기",
+          onPress: () => {},
+        },
+        {
+          text: "완료하기",
+          onPress: () =>
+            navigation.navigate("Main", { id: id, completed: true }),
+        },
+      ]);
+    } else if (time < 0) {
+      setProgress(1);
+      setMinute(parseInt(-time / 60));
+      setSecond(-time % 60);
+    }
+  }, [time]);
+
+  // timerButtons 만들기
+  const timerButtons = [];
+  let i = 1;
+  const r = 135;
+  while (i < 13) {
+    const number = i * 5;
+    const angle = (i * 30 * Math.PI) / 180;
+    const top = 145 - r * Math.cos(angle);
+    const left = 145 + r * Math.sin(angle);
+    const selected = number === minute ? true : false;
+    timerButtons.push(
+      <TimerButton
+        onPress={() => {
+          setTime(number * 60);
+          setInitialTime(number * 60);
+        }}
+        top={top}
+        left={left}
+        selected={selected}
+        key={number}
+      >
+        <ButtonText selected={selected}>{number}</ButtonText>
+      </TimerButton>
+    );
+    i = i + 1;
+  }
+
   };
 
   return (
@@ -64,6 +148,22 @@ const Timer = () => {
           <Progress.Pie progress={progress} size={340} color={theme.red} />
         </PieContainer>
       ) : (
+        <View>
+          <TimerButtonContainer>
+            {timerButtons}
+            <StartButton
+              top={128}
+              left={128}
+              onPress={() => {
+                setUsePie(true);
+                setIsCounting(true);
+              }}
+            >
+              <ButtonText style={{ color: "white" }}>시작</ButtonText>
+            </StartButton>
+          </TimerButtonContainer>
+        </View>
+      )}
       <ClockContainer>
         <ClockNumber>
           {time < 0 && "+"}
