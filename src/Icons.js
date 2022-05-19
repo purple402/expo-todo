cc406b2aaea38375fcefe545d445c3a3482057f5
Joy Lee <purple402@naver.com>
2022-05-19 15:56:28 +0900

    Delete useless icon

--- src/Icons.js
+++ src/Icons.js
@@ -1,11 +1,9 @@
 import Uncheck from '../assets/icons/uncheck.png';
 import Checked from '../assets/icons/checked.png';
-import Edit from '../assets/icons/edit.png';
 import Delete from '../assets/icons/delete.png';
 
 export const Icons = {
   checked: Checked,
   unchecked: Uncheck,
-  edit: Edit,
   delete: Delete,
 };
